3be82abc25243298333573f2dabaa59603a257ab
henryjameslau <henry.james.lau@gmail.com>
2021-05-25 16:08:05 +0100

    first area?

--- postprocessing.js
+++ postprocessing.js
@@ -7,4 +7,4 @@ import { readJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts'
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const json = await readJSON(filename)
-console.log(json[0].url)
+console.log(json[0])
