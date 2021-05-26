920e1f63cf9ae804cc77d476d4f9bc9a752754cc
henryjameslau <henry.james.lau@gmail.com>
2021-05-26 16:44:43 +0100

    geojson to topojson

--- postprocessing.js
+++ postprocessing.js
@@ -2,9 +2,26 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
-import { readJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts'
+import { readJSON,writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts'
+import mapshaper from 'https://cdn.skypack.dev/mapshaper';
+import * as topojson from "topojson";
+
 
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const json = await readJSON(filename)
-console.log(json.services[0].url)
+
+// get a list of areas
+const areas = json.services
+
+//just pick the first area
+const firstarea=areas[0].url+"/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+
+//get the first area as geojson
+const firstGeojson = await readJSON(firstarea)
+
+//convert it to topojson
+const firstTopo = topojson.topology(firstGeojson)
+
+//write file
+await writeJSON('firstTopo.json',firstTopo)
