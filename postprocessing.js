8c2b56b3b02fd588f105317ce975d2834aa2e811
henryjameslau <henry.james.lau@gmail.com>
2021-05-26 16:46:26 +0100

    topojson from skypack

--- postprocessing.js
+++ postprocessing.js
@@ -3,8 +3,8 @@
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 import { readJSON,writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts'
-import mapshaper from 'https://cdn.skypack.dev/mapshaper';
-import * as topojson from "topojson";
+// import mapshaper from 'https://cdn.skypack.dev/mapshaper';
+import topojson from 'https://cdn.skypack.dev/topojson';
 
 
 // Step 1: Read the downloaded_filename JSON
