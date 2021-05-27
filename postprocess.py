32f24175d3dbddb574d431bce098e59831d4230d
henryjameslau <henry.james.lau@gmail.com>
2021-05-27 12:22:44 +0100

    let's try and make a topojson

--- postprocess.py
+++ postprocess.py
@@ -2,8 +2,8 @@ import sys
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import topojson as tp
 import json
-import requests
 
 # Simple script to show the parameters sent to the script, and generate a dummy file
 
@@ -17,9 +17,10 @@ if __name__ == "__main__":
 
     # print(areas)
     # loop through areas
-    for i in areas['services'][0:5]:
-        print(i)
-        geojson=requests.get(i['url']+"/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
-        print(geojson.text)
+    for i in areas['services'][0]:
+        geojson=gpd.read_file(i['url']+"/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+        topojson=tp.Topology(geojson).to_json()
+        with open('topojson.json',w) as f:
+            json.dump(topojson,f)
     # close file
     file.close()
