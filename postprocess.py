fdfc0ad3b4691ea46098131ac431dfdb259a7ab4
henryjameslau <henry.james.lau@gmail.com>
2021-05-27 09:28:59 +0100

    open json

--- postprocess.py
+++ postprocess.py
@@ -2,14 +2,20 @@ import sys
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import json
 
 # Simple script to show the parameters sent to the script, and generate a dummy file
 
 if __name__ == "__main__":
 
     print("argv :", sys.argv)
+    # open file
+    file = open(sys.argv[1])
+    areas=json.load(file)
 
-    df = pd.DataFrame(np.random.randint(
-        0, 100, size=(10, 4)), columns=list('ABCD'))
+    # loop through areas
+    for i in areas['services']
+        print(i)
 
-    df.to_csv("df_output.csv")
+    # close file
+    file.close()
