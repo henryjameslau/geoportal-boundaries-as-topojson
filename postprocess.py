30f430d189e0786e6e67693cef315f4b7a604d48
henryjameslau <henry.james.lau@gmail.com>
2021-05-27 14:38:10 +0100

    0 to 1

--- postprocess.py
+++ postprocess.py
@@ -1,9 +1,7 @@
 import sys
-import pandas as pd
-import numpy as np
 import geopandas as gpd
 import topojson as tp
-import json
+import requests
 
 # Simple script to show the parameters sent to the script, and generate a dummy file
 
@@ -11,11 +9,11 @@ if __name__ == "__main__":
 
     print("argv :", sys.argv)
     # open file
-    file = open(sys.argv[1])
-    areas=json.load(file)
+    file = requests.get(sys.argv[1])
+    areas=file.json()
 
     # loop through areas
-    for i in areas['services'][0]:
+    for i in areas['services'][0:1]:
         geojson=gpd.read_file(i['url']+"/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
         tp.Topology(geojson).to_json('topojson.json')
 
