8fdd7590e30fe7941877f120640832102b264f76
henryjameslau <henry.james.lau@gmail.com>
2022-10-11 10:26:54 +0100

    new endpoint

--- postprocess.py
+++ postprocess.py
@@ -13,7 +13,17 @@ if __name__ == "__main__":
     # open list of boundaries from the geoportal
     file = open(sys.argv[1])
     areas=json.load(file)
-    areas=areas['services']
+    folders=areas['folders']
+
+# blank array to store all the FeatureServers
+    allFeatureServer=[]
+
+    for i in folders:
+        folder=requests.get("https://ons-inspire.esriuk.com/arcgis/rest/services/"+i+"?f=pjson")
+        services=folder.json()['services']
+        filtered=[service for service in services if service['type']=='FeatureServer']
+    #     https://devenum.com/filter-a-list-of-python-dictionaries-by-conditions/
+    allFeatureServer=allFeatureServer+filtered
 
     # find only areas with BUC, BGC
     # https://stackoverflow.com/questions/9012008/pythons-re-return-true-if-string-contains-regex-pattern
@@ -21,44 +31,42 @@ if __name__ == "__main__":
 
     filtered=[]
 
-    for s in areas:
+    for s in allFeatureServer:
         if regexp.search(s['name']):
-            filtered.append([s['name'],s['url']])
+            filtered.append(s['name'])
 
     # loop through areas
     for i in filtered:
 
         # name of boundary
-        print(i[0])
+        print(i.split('/')[1])
 
         # read the file as a geojson
-        geojson=gpd.read_file(i[1]+"/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+        geojson = gpd.read_file("https://ons-inspire.esriuk.com/arcgis/rest/services/"+i+"/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
 
-        # Keep and rename the columns we want and drop any others
+
+        # find the names of the columns that have CD in or NM
         for j in geojson.columns:
-            if re.search(r'(NMW)',j):#drop any NMW
+            if re.search(r'(nmw)',j):#drop any NMW
                 geojson=geojson.drop(columns=[j])
-            if re.search(r'CD',j):#look for fields with CD and rename it
+            if re.search(r'cd',j):#look for fields with CD and rename it
                 geojson=geojson.rename(columns={j:'AREACD'})
-            if re.search(r'(NM)(?!NMW)',j):#look for NM but not NMW and rename it AREANM
+            if re.search(r'(nm)(?!nmw)',j):#look for NM but not NMW and rename it AREANM
                 geojson=geojson.rename(columns={j:'AREANM'})
-            if re.search(r'BNG',j):#drop any with BNG
+            if re.search(r'bng',j):#drop any with BNG
                 geojson=geojson.drop(columns=[j])
-            if re.search(r'LAB',j):
+            if re.search(r'lab',j):
                 geojson=geojson.drop(columns=[j])
             if re.search(r'Shape_',j):#drop any with Shape__
                 geojson=geojson.drop(columns=[j])
-            if j=='LONG' or j=='LAT' or j=='OBJECTID' or j=='FID' or j=='Role' or j=='LEP':#drop any with long, lat, objectid
+            if j=='long' or j=='lat' or j=='objectid' or j=='fid' or j=='role' or j=='lep':#drop any with long, lat, objectid
                 geojson=geojson.drop(columns=[j])
 
-
-        # change projection
-        geojson = geojson.to_crs('EPSG:4326')
 
         #     make a folder if one doesn't exist
         os.makedirs('./outputs',exist_ok=True)
 
-        pathtosave='./outputs/'+i[0]+'.json'
+        pathtosave='./outputs/'+i.replace("/","--")+'.json'
         # convert to topojson and save
         tp.Topology(geojson).to_json(fp=pathtosave)
 
